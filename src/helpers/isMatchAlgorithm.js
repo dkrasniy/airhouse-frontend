972d0886ee4dbf6e01a8ef89e30bc8f574edb5dd
David Krasniy <davidkrasniy@gmail.com>
2022-05-12 19:45:37 -0700

    Update isMatchAlgorithm.js

--- src/helpers/isMatchAlgorithm.js
+++ src/helpers/isMatchAlgorithm.js
@@ -8,14 +8,14 @@ export function isMatchAlgorithm(pattern, word) {
 
             // if not a * or !, exact letter better match
             if (pattern[i] !== '*' && pattern[i] !== '!') {  
-                if (word[i+j] != pattern[i]) return false // if it characters do not match, return false
+                if (word[i+j] !== pattern[i]) return false // if it characters do not match, return false
             }
-            if(pattern[i] == '*'){ 
+            if(pattern[i] === '*'){ 
             // add 1 to index to not count/check current index twice
-                while(word[i+j+1] == word[i]) j++; 
+                while(word[i+j+1] === word[i]) j++; 
             }
             //if current letter are the same, return false otherwise, any letter is okay 
-            if(pattern[i] == "!" && (word[i+j] == word[i+j+1])) return false
+            if(pattern[i] === "!" && (word[i+j] === word[i+j+1])) return false
         }
         return true
     } return false
