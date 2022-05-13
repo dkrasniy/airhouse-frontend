f518de94e9809fdbca7ea6c3d497b63da02a8487
David Krasniy <davidkrasniy@gmail.com>
2022-05-12 18:50:49 -0700

    Setup tailwind, React Router, Framer

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'airhouse': {
+          '50': '#f9f8ff', 
+          '100': '#f3f1ff', 
+          '200': '#e1dbff', 
+          '300': '#cfc5ff', 
+          '400': '#ac9aff', 
+          '500': '#886eff', 
+          '600': '#7a63e6', 
+          '700': '#6653bf', 
+          '800': '#524299', 
+          '900': '#43367d'
+      }
+      }
+    },
+  },
+  plugins: [],
+}
